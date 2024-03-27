7fea4aec75fbd092d251f8f201fa0fb88f18c9df
iara <iarag@LAPTOP-507J9KJ5.(none)>
2024-03-27 17:42:58 -0300

    commit

---
+++ vd_numeros-main/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import * as d3 from "d3"
+  import Batterycharging from "./lib/Batterycharging.svelte"
+  let numbers = [10, 25, 33, 62, 75, 100]
+  let numbersDivided5 = numbers.map(n => Math.floor(n / 5))
+
+  function altura(n) {
+    let scale = d3
+      .scaleLinear()
+      .domain(d3.extent(numbers))
+      .range([15, 225])
+    return scale(n)
+  }
+</script>
+
+<main>
+
+  <h1 class="headline">CARGA DE LA BATERIA</h1>
+  <Batterycharging numbers={numbers}/>
+
+</main>
+
+<style>
+  :global(body){
+    background-color: white;
+  }
+  .headline {
+    position: relative;
+    font-family:Times;
+    font-weight: lighter;
+    text-align: center;
+    margin-top: 100px;
+    bottom: 75px;
+    color: black;
+  }
+
+</style>
---
+++ vd_numeros-main/src/lib/Batterycharging.svelte
@@ -0,0 +1,65 @@
+<script>
+  import * as d3 from "d3"
+
+  export let numbers = []
+
+  function alturaColBattery(n) {
+    let scale = d3
+      .scaleLinear()
+      .domain([0, d3.max(numbers)])
+      .range([0, 100])
+    return scale(n)
+  }
+
+</script>
+
+
+<div class="battery-container">
+  {#each numbers as n}
+  <div>
+    <div class="column-wrapper">
+      <div class="column-battery" style="height: {alturaColBattery(n)}%;"></div>
+      <img class="battery" src="./images/bateria-vacia.png" alt="" style="height: 231px;">
+      <img class="rayo" src="./images/destello.png" alt=""style="height: 60px;">
+    </div>
+    <p class="number">{n}</p>
+  </div>
+  {/each}
+</div>
+
+<style>
+  .battery-container {
+    display: flex;
+  }
+  .column-wrapper {
+    position: relative;
+    width: 160px;
+    height: 200px;
+  }
+  .column-battery {
+    position: absolute;
+    width: 115px;
+    left: 27%;
+    right: 25%;
+    bottom: 0;
+    background-color: rgb(42, 180, 53);
+  }
+  .battery {
+    position: absolute;
+    width: 112%;
+    bottom: -10px;
+  }
+  .rayo {
+    position: absolute;
+    left: 82px;
+    bottom: 72px;
+  }
+  .number {
+    position: relative;
+    font-size: larger;
+    font-weight: 500;
+    text-align: center;
+    left: 12px;
+    color: black;
+  }
+</style>
